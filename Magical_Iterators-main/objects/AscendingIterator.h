040ccd5826846c24fec4e0efe3444ce7cb054918
yuvalbs <yuvalbensimhon33@gmail.com>
2023-05-25 23:16:18 -0700

    Initial commit

---
+++ Magical_Iterators-main/objects/AscendingIterator.h
@@ -0,0 +1,14 @@
+//
+// Created by yuvalbs on 5/25/23.
+//
+
+#ifndef INC_5TASKA_ASCENDINGITERATOR_H
+#define INC_5TASKA_ASCENDINGITERATOR_H
+
+
+class AscendingIterator {
+
+};
+
+
+#endif //INC_5TASKA_ASCENDINGITERATOR_H
