2938789f635ff2d80269252264e3c423efec68d9
yuvalbs <yuvalbensimhon33@gmail.com>
2023-05-27 06:14:04 -0700

    operators >,< need to be implemented.All the rest are working

--- Magical_Iterators-main/Demo.cpp
+++ Magical_Iterators-main/Demo.cpp
@@ -15,37 +15,30 @@ int main() {
     // Use AscendingIterator to display elements in ascending order
     std::cout << "Elements in ascending order:\n";
     AscendingIterator ascIter(container);
-    int * temp=ascIter.begin();
-    //std::cout <<"temp: "<<temp<< std::endl;
-
-    ++temp;
-    int * temp2=ascIter.end();
-    //std::cout <<"temp2: "<<temp2<< std::endl;
-
     for (auto it = ascIter.begin(); it != ascIter.end(); ++it) {
         std::cout << *it << ' ';   // 2 3 9 17 25
     }
     std::cout << std::endl;
 
-//    // Use DescendingIterator to display elements in descending order
-//    std::cout << "Elements in cross order:\n";
-//    SideCrossIterator crossIter(container);
-//    for (auto it = crossIter.begin(); it != crossIter.end(); ++it) {
-//        std::cout << *it << ' ';  // 2 25 3 17 9
-//    }
-//    std::cout << std::endl;
-//
-//    // Use PrimeIterator to display prime numbers only
-//    std::cout << "Prime numbers:\n";
-//    PrimeIterator primeIter(container);
-//    for (auto it = primeIter.begin(); it != primeIter.end(); ++it) {
-//        std::cout << *it << ' ';  // 2 3 17
-//    }
-//    std::cout << std::endl;
-//
-//    // Remove an element from the container and display the size
-//    container.removeElement(9);
-//    std::cout << "Size of container after removing an element: " << container.size() << std::endl;
+    // Use DescendingIterator to display elements in descending order
+    std::cout << "Elements in cross order:\n";
+    SideCrossIterator crossIter(container);
+    for (auto it = crossIter.begin(); it != crossIter.end(); ++it) {
+        std::cout << *it << ' ';  // 2 25 3 17 9
+    }
+    std::cout << std::endl;
+
+    // Use PrimeIterator to display prime numbers only
+    std::cout << "Prime numbers:\n";
+    PrimeIterator primeIter(container);
+    for (auto it = primeIter.begin(); it != primeIter.end(); ++it) {
+        std::cout << *it << ' ';  // 2 3 17
+    }
+    std::cout << std::endl;
+
+    // Remove an element from the container and display the size
+    container.removeElement(9);
+    std::cout << "Size of container after removing an element: " << container.size() << std::endl;
 
     return 0;
 }
